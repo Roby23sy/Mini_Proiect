6a1f1cca3dee7f6c10c8e55739ec9bf1af369885
Roby23sy <roby23sy@yahoo.com>
2022-03-25 07:50:15 +0200

    Responsive-ish

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>Main</title>
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!--Font Awesome-->
     <script src="https://kit.fontawesome.com/1c2cc89c8c.js" crossorigin="anonymous"></script>
     <!--Google Fonts-->
@@ -43,6 +43,7 @@
 </div>
 
 <nav class="navbar navbar-light bg-light">
+
   <div class="container-fluid">
   <button type="button" onclick="menu_apear()" class="button"><i class="fas fa-bars  menu_bars"></i></button>
   <img src="assets/logo-imdb.png" alt="IMDb logo" class="logo" loading="lazy">
@@ -50,78 +51,95 @@
   </div>
 </nav>
 
-<div class="card mx-auto shadow-sm rounded border p-2 overflow-hidden">
-            <div class="row no-gutters" style="display: flex; flex-direction:row;">
-                <div class="col-md-5 pl-3">
-                    <img class="card-img" src="assets/movies-images/space-force.jpg" alt="movie-image"  loading="lazy">
-                </div>
-                <div class="col-md-7 pl-3">
-                    <div class="container"  style="display: flex;flex-direction: column;flex-wrap: wrap;align-content: flex-start; height:520px;">
-                    <div class="flex-item top">
-                        <p class="card-title title">Space Force</p>
-                        <p class="caption-top rating"><i class="fas fa-star fa-lg"></i> 6.9</p>
+<div class="pt-3">
+    <div class="max-w-7xl mx-auto col-sm-6 col-lg-8">
+        <div class="p-2 bg-white overflow-hidden shadow-sm rounded border w-auto">
+            <div class="flex-container">
+                <div class="row">
+                    <div class="flex-item pl-3 align-self-end img">
+                        <img class="card-img" src="assets/movies-images/space-force.jpg" alt="movie-image" loading="lazy">
                     </div>
-
-                    <div class="flex-item mid " >
-                        <p class="card-text-center description"><span id="artists">Director: </span>Robert Zemeckis</p>
-                    </div>
-
-                    <div class="flex-item pl-3 bot">
-                        <button type="button" class="btn btn-primary btn-block" data-bs-toggle="button" autocomplete="off">View more</button>
-                    </div>
+                    <div class="flex-item pl-3" style="width: 68%">
+                        <div class="title">
+                            Space Force
+                        </div>
+                        <div class="pt-2 pb-2">
+                            <i class="fas fa-star text-yellow-400"></i> <span style="font-size: 14px">6.9</span>
+                        </div>
+                        <div class="pt-4 pb-3 description">
+                            <strong>Director:</strong> Robert Zemeckis
+                        </div>
+                        <div class="pb-2 pt-1"></div>
+                        <div class="pt-4" id="button">
+                                <button type="button" class="btn btn-primary btn-block w-100">View More</button>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 </div>
 
-<div class="card mx-auto shadow-sm rounded border p-2 overflow-hidden">
-            <div class="row no-gutters" style="display: flex; flex-direction:row;">
-                <div class="col-md-5 pl-3">
-                    <img class="card-img" src="assets/movies-images/forest-gump.jpg" alt="movie-image " loading="lazy">
-                </div>
-                <div class="col-md-7 pl-3">
-                    <div class="container"  style="display: flex;flex-direction: column;flex-wrap: wrap;align-content: flex-start; height:auto;">
-                    <div class="flex-item top">
-                        <p class="card-title title">Forest Gump</p>
-                        <p class="caption-top rating"><i class="fas fa-star fa-lg"></i> 8.8</p>
+<div class="pt-3">
+    <div class=" mx-auto max-w-7xl col-sm-6 col-lg-8">
+        <div class="p-2 bg-white overflow-hidden shadow-sm rounded border w-auto">
+            <div class="flex-container">
+                <div class="row">
+                    <div class="flex-item pl-3  img">
+                        <img class="card-img" src="assets/movies-images/space-force.jpg" alt="movie-image" loading="lazy">
                     </div>
-
-                    <div class="flex-item mid " >
-                        <p class="card-text-center description"><strong>Director: </strong>Robert Zemeckis</p>
-                        <p class="card-text-center description"><strong>Writers: </strong>Winston Groom</p>
-                        <p class="card-text-center description"><strong>Stars: </strong>Tom Hanks, Robin Wright, Gary Sinise</p>
-                    </div>
-
-                    <div class="flex-item pl-3 bot">
-                        <button type="button" class="btn btn-primary btn-block" data-bs-toggle="button" autocomplete="off">View more</button>
-                    </div>
+                    <div class="flex-item pl-3" style="width: 68%">
+                        <div class="title">
+                            Space Force
+                        </div>
+                        <div class="pt-2 pb-2">
+                            <i class="fas fa-star text-yellow-400"></i> <span style="font-size: 14px">8.8</span>
+                        </div>
+                        <div class="pt-2 pb-2 description" >
+                            <strong>Director: </strong>Robert Zemeckis
+                            <br>
+                            <strong>Writers: </strong>Winston Groom
+                            <br>
+                            <strong>Stars: </strong>Tom Hanks, Robin Wright, Gary Sinise
+                        </div>
+                        <div class="pb-1 pt-1"></div>
+                        <div class="pt-4" id="button">
+                            <button type="button" class="btn btn-primary btn-block w-100">View More</button>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 </div>
 
-<div class="card mx-auto shadow-sm rounded border p-2 overflow-hidden">
-            <div class="row no-gutters" style="display: flex; flex-direction:row;">
-                <div class="col-md-5 pl-3">
-                    <img class="card-img" src="assets/movies-images/joker.jpg" alt="movie-image " loading="lazy">
-                </div>
-                <div class="col-md-7 pl-3">
-                    <div class="container"  style="display: flex;flex-direction: column;flex-wrap: wrap;align-content: flex-start; height:520px;">
-                    <div class="flex-item top">
-                        <p class="card-title title">Joker</p>
-                        <p class="caption-top rating"><i class="fas fa-star fa-lg"></i> 7.0</p>
+<div class="pt-3">
+    <div class="max-w-7xl mx-auto col-sm-6 col-lg-8">
+        <div class="p-2 bg-white overflow-hidden shadow-sm rounded border w-auto">
+            <div class="flex-container">
+                <div class="row">
+                    <div class="flex-item pl-3 align-self-end img">
+                        <img class="card-img" src="assets/movies-images/joker.jpg" alt="movie-image" loading="lazy">
                     </div>
-
-                    <div class="flex-item mid " >
-                        <p class="card-text-center description"><strong>Stars: </strong>Tom Hanks, Robin Wright, Gary Sinise</p>
-                    </div>
-
-                    <div class="flex-item pl-3 bot">
-                        <button type="button" class="btn btn-primary btn-block" data-bs-toggle="button" autocomplete="off">View more</button>
-                    </div>
+                    <div class="flex-item pl-3" style="width: 68%">
+                        <div class="title">
+                            Joker
+                        </div>
+                        <div class="pt-2 pb-2">
+                            <i class="fas fa-star text-yellow-400"></i> <span style="font-size: 14px">7.0</span>
+                        </div>
+                        <div class="pt-4 pb-3 description">
+                            <strong>Stars: </strong>Tom Hanks, Robin Wright, Gary Sinise
+                        </div>
+                        <div class="pb-2 pt-1"></div>
+                        <div class="pt-4" id="button">
+                            <button type="button" class="btn btn-primary btn-block w-100">View More</button>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 </div>
 
 
